fd45929b24a7dfbac091e542bdb90ef3d025860f
Khalid Asseffar <khalid@email.com>
2026-06-08 10:23:37 -0400

    Fix server and database ports

--- server/db.js
+++ server/db.js
@@ -3,12 +3,20 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
+console.log("DB CONFIG:", {
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  database: process.env.DB_NAME,
+  dbPort: process.env.DB_PORT,
+});
+
 export const db = mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   port: Number(process.env.DB_PORT),
+  connectTimeout: 20000,
 });
 
 db.connect((err) => {
--- server/index.js
+++ server/index.js
@@ -34,4 +34,7 @@ app.post("/users", (req, res) => {
 });
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`🚀 Serveur en ligne sur le port ${PORT}`));
+
+app.listen(PORT, () => {
+  console.log(`🚀 Serveur en ligne sur le port ${PORT}`);
+});
